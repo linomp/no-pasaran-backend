7e2f78bfca37d705dfe055cac85b9e848633f38e
linomp <lm2p@hotmail.es>
2023-01-21 21:53:18 +0100

    update build script

--- build.bash
+++ build.bash
@@ -2,10 +2,10 @@ git pull
 
 # This step should be removed... issue is documented here:
 # https://github.com/linomp/no-pasaran-backend/issues/1
-cp /etc/letsencrypt/live/apps.xmp.systems/cert.pem app/cert.pm
-cp /etc/letsencrypt/live/apps.xmp.systems/privkey.pem app/privkey.pm
-chmod 777 app/cert.pm
-chmod 777 app/privkey.pm
+cp /etc/letsencrypt/live/apps.xmp.systems/cert.pem app/cert.pem
+cp /etc/letsencrypt/live/apps.xmp.systems/privkey.pem app/privkey.pem
+chmod 777 app/cert.pem
+chmod 777 app/privkey.pem
 
 docker container stop no-pasaran-backend
 docker container rm no-pasaran-backend
