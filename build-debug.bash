52cff8a4e9c9a49c305107756682741573327d33
linomp <lm2p@hotmail.es>
2023-07-15 13:09:04 +0200

    update docker-compose file

--- build-debug.bash
+++ build-debug.bash
@@ -1,11 +1,11 @@
 git pull
 
-# This step should be removed... issue is documented here:
-# https://github.com/linomp/no-pasaran-backend/issues/1
-cp /etc/letsencrypt/live/apps.xmp.systems/cert.pem app/cert.pem
-cp /etc/letsencrypt/live/apps.xmp.systems/privkey.pem app/privkey.pem
-chmod 777 app/cert.pem
-chmod 777 app/privkey.pem
+## This step should be removed... issue is documented here:
+## https://github.com/linomp/no-pasaran-backend/issues/1
+#cp /etc/letsencrypt/live/apps.xmp.systems/cert.pem app/cert.pem
+#cp /etc/letsencrypt/live/apps.xmp.systems/privkey.pem app/privkey.pem
+#chmod 777 app/cert.pem
+#chmod 777 app/privkey.pem
 
 docker container stop devtest
 docker container rm devtest
