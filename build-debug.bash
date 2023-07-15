8b3cbf48707b3b7191d72b24862675c7813585ba
linomp <lm2p@hotmail.es>
2023-07-15 13:47:54 +0200

    update docker-compose file

--- build-debug.bash
+++ build-debug.bash
@@ -1,16 +1,11 @@
 git pull
 
-## This step should be removed... issue is documented here:
-## https://github.com/linomp/no-pasaran-backend/issues/1
-#cp /etc/letsencrypt/live/apps.xmp.systems/cert.pem app/cert.pem
-#cp /etc/letsencrypt/live/apps.xmp.systems/privkey.pem app/privkey.pem
-#chmod 777 app/cert.pem
-#chmod 777 app/privkey.pem
-
 docker container stop devtest
 docker container rm devtest
 
 docker image rm pointless-backend-debug
 docker build -t pointless-backend-debug -f DockerfileDebug .
 
-docker run -it --name devtest -p 8080:8080 -p 8443:8443 -v /etc/letsencrypt/archive/apps.xmp.systems:/code/app/certs:ro pointless-backend-debug
+docker run -it --name devtest -p 443:443 -v /etc/letsencrypt/archive/apps.xmp.systems:/code/app/certs:ro pointless-backend-debug
+
+# uvicorn app.main:app --host "0.0.0.0" --port 443 --ssl-keyfile=./app/certs/privkey3.pem --ssl-certfile=./app/certs/cert3.pem
--- build-http.bash
+++
@@ -1,9 +0,0 @@
-git pull
-
-docker container stop no-pasaran-backend-http
-docker container rm no-pasaran-backend-http
-
-docker image rm no-pasaran-backend-http
-docker build -t no-pasaran-backend-http -f DockerfileHttp .
-
-docker run -d --name no-pasaran-backend-http -p 80:80 no-pasaran-backend-http
