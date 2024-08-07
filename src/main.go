d232043ddf6bea983c45aaf2662e5bbbc86d5eed
linomp <linomp95@hotmail.com>
2024-08-07 22:03:11 +0200

    remove switch statement

--- src/main.go
+++ src/main.go
@@ -10,22 +10,16 @@ type metricsHandler struct {
 
 func (h *metricsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-	switch {
-	case r.Method == http.MethodGet:
-		metrics := getStatus(r)
-		html := generateServerStatusHTML(metrics)
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(html))
-		return
-	default:
-		return
-	}
+	metrics := getStatus(r)
+	html := generateServerStatusHTML(metrics)
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(html))
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", &metricsHandler{})
+	mux.Handle("GET /", &metricsHandler{})
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:8001", mux))
 }
