88f8bbd75a8d6d0473e4d6b2ba303eeb1bb8589a
linomp <linomp95@hotmail.com>
2024-08-10 13:39:00 +0200

    bring in C code

---
+++ c/server/webserver.c
@@ -0,0 +1,12 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+// maximum application buffer
+#define APP_MAX_BUFFER 1024
+#define PORT 8080
+
+int main() { return 0; }
