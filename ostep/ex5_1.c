52ce485b6f6d68132ec165d35746c62a1f630769
linomp-droplet <linomp95@hotmail.com>
2024-08-23 15:47:10 +0000

    simplify folders

--- ostep/ex5_1.c
+++ ostep/ex5_1.c
@@ -6,19 +6,21 @@
 int main(int argc, char *argv[]){
 
     printf("hello world (pid:%d)\n", (int) getpid());
-    
+
+    int x = 100;
+
     int rc = fork();
 
     if (rc < 0) {
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) {
-        printf("hello, I am child (pid:%d)\n", (int) getpid());
+        printf("hello, I am child (pid:%d) // x=%d\n", (int) getpid(), x);
 	    sleep(1);
     } else {
         int wc = wait(NULL);
-        printf("hello, I am parent of %d (wc:%d) (pid:%d)\n",
-	       rc, wc, (int) getpid());
+        printf("hello, I am parent of %d (wc:%d) (pid:%d)  // x=%d\n",
+	       rc, wc, (int) getpid(), x);
     }
 
     return 0;
