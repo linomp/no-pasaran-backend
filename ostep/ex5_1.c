ab285372661bc3276302ebbb2ee1e4496243de6d
linomp-droplet <linomp95@hotmail.com>
2024-08-23 16:29:53 +0000

    do some exercises

--- ostep/ex5_1.c
+++ ostep/ex5_1.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]){
-
-    printf("hello world (pid:%d)\n", (int) getpid());
-
-    int x = 100;
+int main(int argc, char* argv[]) {
+    int* x = (int*)malloc(sizeof(int));
 
     int rc = fork();
 
-    if (rc < 0) {
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if (rc == 0) {
-        printf("hello, I am child (pid:%d) // x=%d\n", (int) getpid(), x);
-	    sleep(1);
+    if (rc == 0) {
+        for (int i = 0; i <= 10; i++) {
+            printf("child | x=%d\n", (*x)++);
+            usleep(1);
+        }
+
     } else {
-        int wc = wait(NULL);
-        printf("hello, I am parent of %d (wc:%d) (pid:%d)  // x=%d\n",
-	       rc, wc, (int) getpid(), x);
+        for (int i = 0; i <= 10; i++) {
+            printf("parent| x=%d\n", (*x)++);
+            usleep(1);
+        }
+
+        wait(NULL);
     }
 
     return 0;
 }
+
+/*
+Q: Write a program that calls fork(). Before calling fork(), have the
+main process access a variable (e.g., x) and set its value to something (e.g.,
+100). What value is the variable in the child process? What happens to the
+variable when both the child and parent change the value of x?
+
+A:
+No race condition happens because they don't share memory actually.
+Child process gets its own "private memory" (copy of parent's address space)
+Therefore parent & child modify their own copy of x, indepedently.
+
+Things would break if they were using shared memory.
+
+note: Even if x is a pointer, nothing changes!
+*/
