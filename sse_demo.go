c943f83f0e0665a08b0d3d93af69d8836d6509e0
linomp <linomp95@hotmail.com>
2024-09-04 20:46:09 +0200

    add sse demo basic starter

---
+++ sse_demo.go
@@ -0,0 +1,31 @@
+// Source: https://dev.to/gcdcoder/server-sent-events-in-go-an-efficient-real-time-communication-alternative-4obc
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		for {
+			// Simulate sending events every second
+			fmt.Fprintf(w, "data: %s\n\n", time.Now().Format(time.Stamp))
+			w.(http.Flusher).Flush()
+			time.Sleep(1 * time.Second)
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
